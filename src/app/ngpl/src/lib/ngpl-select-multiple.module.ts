1137397f0835263d705dc7f6c52c69efabb38081
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2021-05-28 14:33:29 -0400

    Fixes

--- src/app/ngpl/src/lib/ngpl-select-multiple.module.ts
+++ src/app/ngpl/src/lib/ngpl-select-multiple.module.ts
@@ -8,7 +8,7 @@ import {MatListModule} from '@angular/material/list';
 import {MatSelectModule} from '@angular/material/select';
 import {MatIconModule} from '@angular/material/icon';
 import {OverlayModule} from '@angular/cdk/overlay';
-import {NgplCommonModule} from 'ngpl';
+import {NgplCommonModule} from 'ngpl-common';
 import {NgplSelectMultipleComponent} from './ngpl-select-multiple/ngpl-select-multiple.component';
 import {NgplCommonDirectivesModule} from 'ngpl-common';
 
